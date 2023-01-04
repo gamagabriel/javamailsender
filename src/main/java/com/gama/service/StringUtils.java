00d5d28d783b86c5350a43541c2c3dafa6b9d26f
gabriel.gama <gbr.gama99@gmail.com>
2023-01-03 23:36:29 -0300

    Create mail sender with JavaMail

---
+++ src/main/java/com/gama/service/StringUtils.java
@@ -0,0 +1,9 @@
+package com.gama.service;
+
+public class StringUtils {
+
+
+    public final static String TO_EMAIL = "EMAIL";
+    public final static String SUBJECT = "HEADER";
+    public final static String OBJECT = "BODY EMAIL";
+}
