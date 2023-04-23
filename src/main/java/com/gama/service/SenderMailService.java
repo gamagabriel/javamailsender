c97b1b82fbacc9205b175e9da139f98be9c3e9a7
gabriel.gama <gbr.gama99@gmail.com>
2023-04-22 23:10:48 -0300

    chore: refactor

--- src/main/java/com/gama/service/SenderMailService.java
+++ src/main/java/com/gama/service/SenderMailService.java
@@ -7,6 +7,8 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import static net.logstash.logback.argument.StructuredArguments.kv;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -15,14 +17,29 @@ public class SenderMailService {
     private final JavaMailSender mailSender;
 
     public void sender(MailPayload mailPayload) {
-        log.info("[SenderMailService] Starting process to send mail to {}", mailPayload.getToEmail());
+        log.info("[sender] Starting process to send mail. {}",
+                kv("receiver", mailPayload.getToEmail()));
+
+        SimpleMailMessage message = buildMessage(mailPayload);
+
+        try {
+            mailSender.send(message);
+        } catch (Exception e) {
+            log.error("[sender] error when trying to send email. {}",
+                    kv("receiver", mailPayload.getToEmail()));
+            throw new RuntimeException(e);
+        }
+        log.info("[sender] mail sended with successfull");
+
+    }
+
+    private SimpleMailMessage buildMessage(MailPayload mailPayload) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(mailPayload.getToEmail());
         message.setCc(mailPayload.getCc());
         message.setText(mailPayload.getObject());
         message.setSubject(mailPayload.getSubject());
-
-        mailSender.send(message);
-        log.info("[sender] mail sended with successfull");
+        return message;
     }
+
 }
